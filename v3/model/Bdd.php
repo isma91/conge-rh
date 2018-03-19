0b634fe287af29dfb81bb1d0ec0ac57a0d8b2638
isma91 <ismaydogmus@gmail.com>
2018-03-19 23:25:05 +0100

    removing all function for the User class

--- v3/model/Bdd.php
+++ v3/model/Bdd.php
@@ -43,45 +43,4 @@ Class Bdd
         return $this->_bdd;
     }
 
-    public function getData() {
-        $data = self::getBdd()->prepare("SELECT id, firstName, lastName, address, dateBegin, conges.acquis, conges.pris FROM salaries INNER JOIN conges ON conges.salaries_id = salaries.id");
-        $data->execute();
-        $result = $data->fetchAll(\PDO::FETCH_ASSOC);
-        return $result;
-    }
-
-    public function getAllById($id) {
-        $data = self::getBdd()->prepare("SELECT id, firstName, lastName, address, dateBegin, conges.acquis, conges.pris FROM salaries INNER JOIN conges ON conges.salaries_id = salaries.id WHERE salaries.id = :id");
-        $data->bindParam(":id", $id);
-        $data->execute();
-        $result = $data->fetchAll(\PDO::FETCH_ASSOC);
-        return $result;
-    }
-    
-    public function updateCongeAcquis($id, $conge) {
-        $id = (int)$id;
-        $conge = (int)$conge;
-        if (!is_int($id) && !is_int($conge)) {
-            return false;
-        }
-        $update = self::getBdd()->prepare("UPDATE conges SET acquis = :acquis WHERE salaries_id = :id");
-        $update->bindParam(":acquis", $conge);
-        $update->bindParam(":id", $id);
-        $update->execute();
-        return $update;
-    }
-    
-    public function updateCongePris($id, $conge) {
-        $id = (int)$id;
-        $conge = (int)$conge;
-        if (!is_int($id) && !is_int($conge)) {
-            return false;
-        }
-        $update = self::getBdd()->prepare("UPDATE conges SET pris = :pris WHERE salaries_id = :id");
-        $update->bindParam(":pris", $conge);
-        $update->bindParam(":id", $id);
-        $update->execute();
-        return $update;
-    }
-
 }
