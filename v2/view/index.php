7ef77b6ff3ff1db040329c4abdd2daac24411e9f
isma91 <ismaydogmus@gmail.com>
2018-03-19 19:43:49 +0100

    adding the display of all data with the update button

---
+++ v2/view/index.php
@@ -0,0 +1,57 @@
+<?php
+require_once("../autoload.php");
+use model\Bdd;
+$bdd = new Bdd();
+$data = $bdd->getData();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="" />
+    <title>Congé RH</title>
+    <link media="all" type="text/css" rel="stylesheet" href="../media/css/font.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="../media/css/materialize.min.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="../media/css/google_material_icons.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="../media/css/style.css" />
+</head>
+<body>
+<div class="container">
+    <div class="row center">
+        <h1>Welcome to Congé RH !!</h1>
+    </div>
+</div>
+<div class="container">
+    <table class="highlight centered">
+        <thead>
+            <tr>
+                <th>Prenom</th>
+                <th>Nom</th>
+                <th>Adresse</th>
+                <th>Date d'entrée</th>
+                <th>Congés acquis</th>
+                <th>Congés pris</th>
+                <th>Modifier</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            foreach ($data as $array) {
+        ?>
+                <tr>
+                <td><?php echo $array["firstName"]; ?></td>
+                <td><?php echo $array["lastName"]; ?></td>
+                <td><?php echo $array["address"]; ?></td>
+                <td><?php echo $array["dateBegin"]; ?></td>
+                <td><?php echo $array["acquis"]; ?></td>
+                <td><?php echo $array["pris"]; ?></td>
+                <td><a href="conge.php?id=<?php echo $array['id']; ?>">Modifier</a></td>
+                </tr>
+        <?php
+            }
+        ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
