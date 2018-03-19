fcccf6c49f4749064721af01b75a2ce793b62a11
isma91 <ismaydogmus@gmail.com>
2018-03-19 16:47:24 +0100

    adding the view to display

---
+++ v1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="" />
+    <title>Congé RH</title>
+    <link media="all" type="text/css" rel="stylesheet" href="media/css/font.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="media/css/materialize.min.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="media/css/google_material_icons.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="media/css/style.css" />
+</head>
+<body>
+</body>
+</html>
