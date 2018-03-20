5e922291672ac65b0e8bf4506c5afa566908dd05
isma91 <ismaydogmus@gmail.com>
2018-03-20 01:48:19 +0100

    fix error for getLastId & the date + we get the newcomer in the getData function

--- v3/model/User.php
+++ v3/model/User.php
@@ -17,7 +17,7 @@ Class User
 
     public function getData() {
         $bdd = new Bdd();
-        $data = $bdd->getBdd()->prepare("SELECT id, firstName, lastName, address, dateBegin, conges.acquis, conges.pris FROM salaries INNER JOIN conges ON conges.salaries_id = salaries.id");
+        $data = $bdd->getBdd()->prepare("SELECT id, firstName, lastName, address, dateBegin, conges.acquis, conges.pris, newcomer FROM salaries INNER JOIN conges ON conges.salaries_id = salaries.id");
         $data->execute();
         $result = $data->fetchAll(\PDO::FETCH_ASSOC);
         return $result;
@@ -25,7 +25,7 @@ Class User
 
     public function getAllById($id) {
         $bdd = new Bdd();
-        $data = $bdd->getBdd()->prepare("SELECT id, firstName, lastName, address, dateBegin, conges.acquis, conges.pris FROM salaries INNER JOIN conges ON conges.salaries_id = salaries.id WHERE salaries.id = :id");
+        $data = $bdd->getBdd()->prepare("SELECT id, firstName, lastName, address, dateBegin, conges.acquis, conges.pris FROM salaries INNER JOIN conges ON conges.salaries_id = salaries.id WHERE salaries.id = :id AND salaries.newcomer = 0");
         $data->bindParam(":id", $id);
         $data->execute();
         $result = $data->fetchAll(\PDO::FETCH_ASSOC);
@@ -63,17 +63,22 @@ Class User
     public function add($firstname, $lastname, $address) {
         $bdd = new Bdd();
         $date = date("Y-m-d");
-        $addUser = $bdd->getBdd()->prepare("INSERT INTO salaries (firstName, lastName, address, dateBegin, dateEnd)
-         VALUES (:firstname, :lastname, :address, :dateBegin, :dateEnd)");
+        /*
+         je n'arrive pas à ajouter pour `dateEnd` 0000-00-00 
+         vu que ce champ est de type 'date' et qu'il ne peut pas être NULL
+         et j'ai du ajouter la current date sur ce champ pour que l'INSERT pouisse passer sans erreur
+         (php 7.0.28 & mysql 14.14 Distrib 5.7.21 for linux)
+        */
+        $addUser = $bdd->getBdd()->prepare("INSERT INTO salaries (firstName, lastName, address, dateBegin, dateEnd) VALUES (:firstname, :lastname, :address, :dateBegin, :dateEnd)");
         $addUser->bindParam(":firstname", $firstname);
         $addUser->bindParam(":lastname", $lastname);
         $addUser->bindParam(":address", $address);
         $addUser->bindParam(":dateBegin", $date);
-        $addUser->bindParam(":dateEnd", "0000-00-00");
+        $addUser->bindParam(":dateEnd", $date);
         $addUser->execute();
         if ($addUser) {
-            $id = $bdd->lastInsertId();
-            $addConge = $bdd->prepare("INSERT INTO conges (salaries_id, acquis, pris) VALUES (:id, 0, 0)");
+            $id = $bdd->getBdd()->lastInsertId();
+            $addConge = $bdd->getBdd()->prepare("INSERT INTO conges (salaries_id, acquis, pris) VALUES (:id, 0, 0)");
             $addConge->bindParam(":id", $id);
             $addConge->execute();
             return $addConge;
