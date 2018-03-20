b49f32c5d1c191f9d38d0b46879a2821cabafd4c
isma91 <ismaydogmus@gmail.com>
2018-03-20 17:21:14 +0100

    adding the click for the form and the akax call

--- v4/media/js/register.js
+++ v4/media/js/register.js
@@ -2,15 +2,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var xhr = new XMLHttpRequest();
     var error = document.getElementById("error");
     var success = document.getElementById("success");
-    
-    xhr.open('POST', '../public_api/index.php');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send('action=');
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.response);
-        } else {
-            error.innerHTML = "Une erreur est survenue lors de la demande de recuperation de l'utilisateur !!";
+    var firstname = lastname = address = "";
+
+    document.getElementById("createUser").addEventListener('click', function (event) {
+        event.preventDefault();
+        firstname = document.getElementById("firstname").value.trim();
+        lastname = document.getElementById("lastname").value.trim();
+        address = document.getElementById("address").value.trim();
+        xhr.open('POST', '../public_api/index.php');
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send('action=createUser&firstname=' + firstname + '&lastname=' + lastname + '&address=' + address);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.response);
+                if (data.error == "") {
+                    success.innerHTML = "Utilisateur avec success !!";
+                }
+            } else {
+                error.innerHTML = "Une erreur est survenue lors de l'ajout de l'utilisateur !!";
+            }
         }
-    };
+    })
 });
