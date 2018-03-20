7dc6a3a8518d1d1c768b95ebff9ed77fe227b283
isma91 <ismaydogmus@gmail.com>
2018-03-20 17:59:57 +0100

    adding the api who redirect to the right function

---
+++ v5/public_api/index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* Index.php
+*
+* All ajax request go here and be sended to different Controller
+*
+* PHP 7.0.28
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+session_start();
+require '../autoload.php';
+use controller\UserController;
+$user = new UserController();
+switch ($_POST["action"]) {
+    case 'getData':
+    $data = $user->getData();
+    break;
+    case 'getAllById':
+    $data = $user->getAllById($_POST["id"]);
+    break;
+    case 'updateAcquis':
+    $data = $user->updateAcquis($_POST["acquis"], $_POST["id"]);
+    break;
+    case 'updatePris':
+    $data = $user->updatePris($_POST["pris"], $_POST["id"]);
+    break;
+    case 'createUser':
+    $data = $user->createUser($_POST["firstname"], $_POST["lastname"], $_POST["address"]);
+    break;
+    case 'deleteUser':
+    $data = $user->deleteUser($_POST["id"]);
+    break;
+}
