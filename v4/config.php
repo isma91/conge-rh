8cc0f3dda5b7c65c0527315873ac59ae09174fa1
isma91 <ismaydogmus@gmail.com>
2018-03-20 13:11:50 +0100

    adding the config file

---
+++ v4/config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * This is a sample of the config file, rename it to config.php
+ *
+ */
+return [
+    'host'        => 'localhost',
+    'dbname'      => '',
+    'user'        => '',
+    'password'    => ''
+];
