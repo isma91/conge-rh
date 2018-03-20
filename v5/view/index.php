11bea457779cadc77dec53a29a39045d2ec2caca
isma91 <ismaydogmus@gmail.com>
2018-03-20 18:00:17 +0100

    adding the view who display all users

---
+++ v5/view/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="" />
+    <title>Congé RH</title>
+    <link media="all" type="text/css" rel="stylesheet" href="../media/css/font.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="../media/css/materialize.min.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="../media/css/google_material_icons.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="../media/css/style.css" />
+    <script src="../media/js/index.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row center">
+        <h1>Welcome to Congé RH !!</h1>
+    </div>
+</div>
+<div class="container">
+    <nav>
+        <div class="nav-wrapper">
+            <ul>
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="register.php">Add User</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<div class="failed" id="error"></div>
+<div class="success" id="success"></div>
+<div class="container">
+    <table class="highlight centered">
+        <thead>
+            <tr>
+                <th>Prenom</th>
+                <th>Nom</th>
+                <th>Adresse</th>
+                <th>Date d'entrée</th>
+                <th>Congés acquis</th>
+                <th>Congés pris</th>
+                <th>Modifier</th>
+                <th>Supprimer</th>
+            </tr>
+        </thead>
+        <tbody id="users"></tbody>
+    </table>
+</div>
+</body>
+</html>
