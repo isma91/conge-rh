a77fad6e2ec015db213d61c6537dfc20bfd68281
isma91 <ismaydogmus@gmail.com>
2018-03-20 13:12:29 +0100

    adding the user controller

---
+++ v4/controller/UserController.php
@@ -0,0 +1,56 @@
+<?php
+namespace controller;
+/**
+ * UserController.php
+ *
+ *
+ * PHP 7.0.28
+ *
+ * @category Model
+ * @package  Model
+ * @author   isma91 <ismaydogmus@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+use model\User;
+/**
+ * Class UserController
+ * @package controller
+ */
+class UserController
+{
+
+    public function add($firstname, $lastname, $address) {
+        $errField = array();
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
+        $address = $_POST["address"];
+        $allField = [
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'address' => $address
+        ];
+        foreach($allField as $filed => $value) {
+            if (empty(trim($value))) {
+                array_push($errField, $value);
+            }
+        }
+        if (count($errField) > 0) {
+            $fields = "";
+            foreach($errField as $key => $value) {
+                $fields = $fields . ", " . $value;
+            }
+            $fields = substr($fields, 2);
+            return array('error' => "These fields are empty: " . $fields . " !!", "success" => "");
+        } else {
+            $user = new User();
+            $add = $user->add($firstname, $lastname, $address);
+            if ($add) {
+                return array("error" => "", "success" => "");
+            } else {
+                return array("error" => 'There is a problem when we try to add the new user in the database !!', 'success' => '');
+            }
+        }
+    }
+
+
+}
